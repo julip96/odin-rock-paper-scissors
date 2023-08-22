e4d0a60a3e6c6213595d3513be557cdd0cb7987a
julip96 <julianporanski@gmail.com>
2023-08-22 10:51:27 +0200

    Added buttons

--- script.js
+++ script.js
@@ -1,7 +1,68 @@
+
+const bod = document.body;
+const buttons = document.createElement('div');
+buttons.classList.add('buttons');
+bod.appendChild(buttons);
+
+function createButton (id) {
+    const but = document.createElement('button');
+    but.classList.add('but');
+    but.textContent = id;
+    but.id = id;
+    buttons.appendChild(but);    
+}
+
+createButton ("rock");
+createButton ("paper");
+createButton ("scissors");
+
+// I want to add a score -> how many wins does computer/human have
+// And I want to add "state a winner after 5 games"
+
+let humanWins = 0;
+let computerWins = 0;
+
+const content = document.createElement('div');
+content.classList.add('content');
+bod.appendChild(content);
+
+// Here we can later add the amount of wins the computer got
+const computer = document.createElement('p');
+computer.classList.add('computer');
+computer.textContent = `Computer Wins: ${computerWins}`;
+content.appendChild(computer);
+
+// same but for the human
+const human = document.createElement('p');
+human.classList.add('human');
+human.textContent = `Human Wins: ${humanWins}`;
+content.appendChild(human);
+
+// Click to play a round against the computer
+const btn = document.querySelectorAll('.but');
+btn.forEach(btn => btn.addEventListener('click', () => {
+    playRound(btn.id, getComputerChoice());
+    if(humanWins == 5)
+    {
+        content.removeChild(computer);
+        human.textContent = "Human Won!";
+        bod.removeChild(buttons);
+        return;
+    }
+    else if(computerWins == 5)
+    {
+        content.removeChild(human);
+        computer.textContent = "Computer Won!";
+        bod.removeChild(buttons);
+        return;
+    }
+}))
+
+
 function getComputerChoice() {
     let random = Math.random() * 2;
     random = Math.round(random);
-    const choices = ["rock", "paper", "scissorcs"];
+    const choices = ["rock", "paper", "scissors"];
     const computerSelection = choices[random];
     return computerSelection;
 }
@@ -13,8 +74,14 @@ function getPlayerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    
+    // log to see the buttons working
+    console.log(computerSelection);
+    console.log(playerSelection);
+
     if(playerSelection === computerSelection)
     {
+        // console logs can be changed to add the value to see who won
         return "tie";
     }
     else if(
@@ -23,14 +90,21 @@ function playRound(playerSelection, computerSelection) {
         (playerSelection == "paper" && computerSelection == "scissors")
         ) 
     {
+        // here aswell
+        computerWins += 1;
+        computer.textContent = `Computer Wins: ${computerWins}`;
         return "lose";
     } 
     else
     {
+        // and here too
+        humanWins += 1;
+        human.textContent = `Human Wins: ${humanWins}`;
         return "victory";
     }  
 }
 
+/* deactivated function because of step 1 in assignment
 function game() {
 
     let playerWins = 0;
@@ -64,3 +138,7 @@ function game() {
 }
 
 game();
+
+*/
+
+
